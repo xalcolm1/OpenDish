d0d82da1f37957c5074c2435ba529f79ae1c244f
Malcolm Reyes <mreyesx99@gmail.com>
2020-10-17 17:31:56 -0700

    Debug heroku

--- frontend/components/sessions/session_form.jsx
+++ frontend/components/sessions/session_form.jsx
@@ -14,7 +14,9 @@ class sessionForm extends React.Component {
     }
 
     handleInput(field) {
+        
       return  e => {
+          
           this.setState({ [field]: e.target.value });
       }
     }
@@ -53,55 +55,87 @@ class sessionForm extends React.Component {
     }
 
     handleClick() {
+        debugger
         return e => {
-            if(e.currentTarget.className !== this.props.classname) {
-                this.props.history.push('/')
+            if(e.currentTarget.className === this.props.classname) {
+            }else{
+                
+                e.stopPropagation()
+            this.props.history.push('/')
             }
-            
         }
     }
 
     
     render() {
        
-        const { formType } = this.props;
+    const { formType } = this.props;
         return (
             <div className="form-backdrop" onClick={this.handleClick()}>
                 <form onSubmit={this.handleSubmit} className={this.props.classname}>
                     
-                    
-                       {
+                    {
                         (formType === "Sign Up") ? (
                             <>
                                 <h1>Welcome to Table Ouverte!</h1>
                                 {this.renderErrors()}
-                                    <input className="text-input" type="text" onChange={this.handleInput('firstname')} value={this.state.firstname} placeholder="Firstname" />
+                                    <input 
+                                        className="text-input"
+                                        type="text" 
+                                        onChange={this.handleInput('firstname')} 
+                                        value={this.state.firstname} 
+                                        placeholder="Firstname" 
+                                    />                                
                                 
-
-                                
-                                    <input className="text-input" type="text" onChange={this.handleInput('lastname')} value={this.state.lastname} placeholder="Lastname "/>
-                                     
+                                    <input 
+                                        className="text-input" 
+                                        type="text" 
+                                        onChange={this.handleInput('lastname')} 
+                                        value={this.state.lastname} 
+                                        placeholder="Lastname "
+                                    />
+                                        
                             </>
                         ) : (
-                        <>
-                            <h1>Please sign in</h1>
-                           {this.renderErrors()}
-                        
-
-                        
-                            <input  type="submit" value="Demo" className="demoBTN" onClick={this.handleDemo }/>
-                        </> 
+                            <>
+                                <h1>Please sign in</h1>
+                            {this.renderErrors()}                       
+                                <input  
+                                    type="submit" 
+                                    value="Demo" 
+                                    className="demoBTN" 
+                                    onClick={this.handleDemo }
+                                />
+                            </> 
                         )
-                        }
-                        <input className="text-input" type="text" onChange={this.handleInput('email')} value={this.state.email} placeholder="Email"/>
+                    }
+                    <input 
+                        className="text-input" 
+                        type="text" 
+                        onChange={this.handleInput('email')} 
+                        value={this.state.email} 
+                        placeholder="Email"
+                    />                    
                     
-
+                    <input 
+                        className="text-input" 
+                        type="password" 
+                        onChange={this.handleInput('password')} 
+                        value={this.state.password} 
+                        placeholder="Password"
+                    />
                     
-                        <input className="text-input" type="password" onChange={this.handleInput('password')} value={this.state.password} placeholder="Password"/>
-                    
-
                     <input type="submit" value={formType} className="submitBTN" />
-                    {(formType === "Sign In") ? (<div>New to Table Ouverte? <Link to="/signup" className="createAnAccount">Create an account</Link></div>): null}
+                    {
+                        (formType === "Sign In") ? (
+                        <div>
+                            New to Table Ouverte? 
+                            <Link  to="/signup" className="createAnAccount" >
+                                Create an account
+                            </Link>
+                        </div>
+                        ): null
+                    }
                 </form>   
             </div> 
          )  
