27f594f75e1e3992920867461289c9b3de4f1fd3
Malcolm Reyes <mreyesx99@gmail.com>
2020-12-11 13:59:04 -0500

    validations fix

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -2,7 +2,12 @@ class Api::ReviewsController < ApplicationController
     before_action :require_logged_in, only: [:create, :destroy]
 
     def create   
-        
+        review = Review.new(review_params)
+        if review.save
+            
+        else
+
+        end
     end
 
     def destroy
