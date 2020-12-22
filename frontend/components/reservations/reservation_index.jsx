bc94873c26839db59275115692227b598ad72841
Malcolm Reyes <mreyesx99@gmail.com>
2020-12-21 22:51:15 -0500

    add style to review index

--- frontend/components/reservations/reservation_index.jsx
+++ frontend/components/reservations/reservation_index.jsx
@@ -3,17 +3,23 @@ import React from 'react';
 
 const ReservationIndex = props => {
     return (
-        <ul>
+        <ul className="reservation-index">
             {props.currentUser.reservations.map((reservation, idx) => {
                 return (
-                    <li key={idx}>
-                        {props.currentUser.firstname}
-                        {reservation.date}
+                    <li key={idx} className="reservation-index-item">
+                        <div>{props.currentUser.firstname}</div>
+                        <div>{reservation.date}</div>
+                        
                      </li>
                 )
             })}
         </ul>
     )
 };
+
+// is this the best place to decide if the reservatoi\on needs to ge moved to the next location.
+
+// what needs to happen next : 
+// have the number of people appear  on every reservation
 
 export default ReservationIndex;
